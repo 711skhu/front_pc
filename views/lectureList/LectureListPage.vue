e827e3b518f033824b473910c0749b766b4acc96
jeenie <jeenie828@gmail.com>
2019-05-29 21:57:55 +0900

    강의 목록 페이지 구성

---
+++ views/lectureList/LectureListPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header-menu></header-menu>
+    <lecture-list></lecture-list>
+  </div>
+</template>
+
+<script>
+  import HeaderMenu from '@/components/header/HeaderManu'
+  import LectureList from '@/components/lecture/LectureList'
+
+  export default {
+    components: {
+      HeaderMenu,
+      LectureList
+    }
+  }
+</script>
+
+<style scoped>
+</style>
