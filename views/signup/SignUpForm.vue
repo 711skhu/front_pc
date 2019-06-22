87191f35c5e08279bf4d07ecae6d7bcfc4e2a263
jeenie <jeenie828@gmail.com>
2019-06-22 21:52:47 +0900

    #14 회원가입 페이지 메인 헤더바 추가

--- views/signup/SignUpForm.vue
+++ views/signup/SignUpForm.vue
@@ -1,60 +1,68 @@
 <template>
-  <div class="content">
-    <h2 class="padding-30p">계정 만들기</h2>
-    <form class="ui form">
-      <div class="field">
-        <label>이름</label>
-        <input v-model="userId" type="text" placeholder="아이디">
-      </div>
-      <div class="field">
-        <label>닉네임</label>
-        <input v-model="nickName" type="text" placeholder="닉네임">
-      </div>
-      <div class="field">
-        <label>이메일</label>
-        <input v-model="email" type="email" placeholder="이메일">
-      </div>
-      <div class="field">
-        <label>비밀번호</label>
-        <input v-model="password" type="password" placeholder="비밀번호">
-      </div>
-      <div class="field">
-        <label>비밀번호 확인</label>
-        <input v-model="passwordCheck" type="password" placeholder="비밀번호 확인">
-        <div v-if="!isPasswordCorrect" class="ui pointing red basic label">
-          비밀번호가 일치하지 않습니다.
+  <div>
+    <header-menu></header-menu>
+    <div class="content">
+      <h2 class="padding-30p">계정 만들기</h2>
+      <form class="ui form">
+        <div class="field">
+          <label>이름</label>
+          <input v-model="userId" type="text" placeholder="아이디">
         </div>
-      </div>
+        <div class="field">
+          <label>닉네임</label>
+          <input v-model="nickName" type="text" placeholder="닉네임">
+        </div>
+        <div class="field">
+          <label>이메일</label>
+          <input v-model="email" type="email" placeholder="이메일">
+        </div>
+        <div class="field">
+          <label>비밀번호</label>
+          <input v-model="password" type="password" placeholder="비밀번호">
+        </div>
+        <div class="field">
+          <label>비밀번호 확인</label>
+          <input v-model="passwordCheck" type="password" placeholder="비밀번호 확인">
+          <div v-if="!isPasswordCorrect" class="ui pointing red basic label">
+            비밀번호가 일치하지 않습니다.
+          </div>
+        </div>
 
-      <div class="field">
-        <div class="ui checkbox">
-          <input
-            type="checkbox"
-            tabindex="0"
-            v-model="checked"
-            true-value="true"
-            false-value="false">
-          <label><strong>이용약관</strong> 및 <strong>개인정보 처리방침</strong>에 동의합니다.</label>
+        <div class="field">
+          <div class="ui checkbox">
+            <input
+              type="checkbox"
+              tabindex="0"
+              v-model="checked"
+              true-value="true"
+              false-value="false">
+            <label><strong>이용약관</strong> 및 <strong>개인정보 처리방침</strong>에 동의합니다.</label>
+          </div>
         </div>
+        <div class="field">
+          <button
+            type="submit"
+            class="ui primary button"
+            :class="{disabled: isDisabledRegister}">
+            계정 만들기
+          </button>
+        </div>
+      </form>
+      <br/>
+      <div class="ui login">
+        이미 계정이 있으신가요? <a href="#">로그인</a>
       </div>
-      <div class="field">
-        <button
-          type="submit"
-          class="ui primary button"
-          :class="{disabled: isDisabledRegister}">
-          계정 만들기
-        </button>
-      </div>
-    </form>
-    <br/>
-    <div class="ui login">
-      이미 계정이 있으신가요? <a href="#">로그인</a>
     </div>
   </div>
 </template>
 
 <script>
+  import HeaderMenu from '@/components/header/HeaderMenu'
+
   export default {
+    components: {
+      HeaderMenu
+    },
     data() {
       return {
         userId: '',
@@ -84,7 +92,7 @@
 
 <style scoped>
   .content {
-    margin: 0 35%;
+    margin: 3% 35%;
   }
 
   .button {
